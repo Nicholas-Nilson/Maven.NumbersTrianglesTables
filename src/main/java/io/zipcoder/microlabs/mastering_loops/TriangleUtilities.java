5f6cba4cd0636cc6b7e966df912dec1457882990
Nick Nilson <nilson.nicholas@gmail.com>
2022-06-27 15:42:04 -0400

    two methods written & passesd

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -1,13 +1,33 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.util.stream.IntStream;
+
 public class TriangleUtilities {
 
+    int width;
+
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String row = "";
+        String triangle = "";
+        String newLine = "\n";
+        char star = '*';
+
+        for (int i = 1; i < numberOfRows; i++) {
+            row = row + star;
+            triangle = triangle + row + newLine;
+        }
+
+        return triangle;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = "";
+        char star = '*';
+        for (int i =0; i < numberOfStars; i++) {
+            row = row + star;
+        }
+        return row;
+//        IntStream.range(0, numberOfStars).forEach(i -> row = row + star); //doesn't work, lambda wants final values.
     }
 
     public static String getSmallTriangle() {
